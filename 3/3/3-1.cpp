14f8693e368e005681baa637f0ffcb53dff3f812
Hemin Qaradagi <hq423@mail.umkc.edu>
2013-06-27 09:26:19 -0500

    Labs 1, 3, and 4

---
+++ 3/3/3-1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+
+
+	string title, author;
+	int isbn;
+	ifstream fileName;
+
+	char fileinput[100];
+	fileName.open("books.txt");
+	fileName.open("isbn.txt");
+	fileName.getline(fileinput, 100);
+
+	title = string(fileinput);
+	book;
+	book.settle(title);
+	librarybook arraybook[20];
+
+	
+	class Books
+	{
+	private: 
+		string title;
+		string authour;
+		int isbn;
+
+	public:
+		void setTitle(string title);
+		void getTitle();
+
+	}
+
+
+
+
+
+	
+	if (bool title; author == true);
+
+
+
+
+
+}
