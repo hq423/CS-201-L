bbce629aadc8a39b9ca7f2e19ce2350cf376be77
unknown <hq423@KC-SCE462LAB-10.kc.umkc.edu>
2013-07-08 11:01:35 -0500

    Midterm

---
+++ midterm/Items.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+class Item {
+
+public:
+	Item()
+	Item(string name, double price):
+	void setName(string name):
+	string getName()
+	void setPrice(double price):
+	double getPrice():
+
+private:
+	string m_name;
+	double m_price;
+
+
+
+
+
+
+
+}
+
+
+
+
